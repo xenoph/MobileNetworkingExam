a700f8c71836159d98e43d4253c9a4f440ee48ce
Bjørn Vidar Dahle <bvidar@dahle.me>
2017-11-22 19:50:03 +0100

    added initial network server methods

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	private NetworkController _nController;
+
+	private void Start() {
+		_nController = GetComponent<NetworkController>();
+	}
+	
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.A)) {
+			_nController.CheckForUsers();
+		}
+		if(Input.GetKeyUp(KeyCode.M)) {
+			_nController.MatchWithOtherPlayer();
+		}
+	}
+}
---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkController : MonoBehaviour {
+
+	public SocketIOComponent Socket;
+
+	private string _socketID;
+	//private void _playerID; maybe?
+
+	private void Start(){
+		Socket = GetComponent<SocketIOComponent>();
+		SetSocketConnections();
+	}
+
+	public void CheckForUsers() {
+		Debug.Log("Checking for users");
+		var json = CreateJSON();
+		Socket.Emit("CheckCurrentUsers", json);
+	}
+	
+	public void MatchWithOtherPlayer() {
+		var json = CreateJSON();
+		Socket.Emit("MatchPlayers", json);
+	}
+
+	private void SetSocketConnections() {
+		Socket.On("init", OnInit);
+		Socket.On("registered", OnRegistered);
+		Socket.On("allPlayers", OnGettingUsers);
+		Socket.On("noMatch", OnNoMatch);
+		Socket.On("matchedPlayer", OnMatchedPlayer);
+	}
+
+	private void OnInit(SocketIOEvent obj) {
+		if(Socket != null) {
+			var json = new JSONObject();
+			Socket.Emit("PlayerLogin", json);
+		}
+	}
+
+	private void OnRegistered(SocketIOEvent obj) {
+		_socketID = obj.data["socketid"].str;
+		Debug.Log(_socketID);
+	}
+
+	private void OnGettingUsers(SocketIOEvent obj) {
+		Debug.Log(obj.data);
+	}
+
+	private void OnNoMatch(SocketIOEvent obj) {
+		Debug.Log("Did not find a match");
+	}
+
+	private void OnMatchedPlayer(SocketIOEvent obj) {
+		Debug.Log("Found a match");
+	}
+
+	private JSONObject CreateJSON() {
+		var json = new JSONObject();
+		json.AddField("SocketID", _socketID);
+		return json;
+	}
+}
